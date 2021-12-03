37015d476a30258f262b2841a519ef0ca54c8fac
anshwhitehtjr <kalpaiteansh@gmail.com>
2021-12-03 16:16:15 +0530

    Very First Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Navbar from '../Components/Navbar';
+import '../styles/tailwindcss.css';
+import Link from 'next/link';
+import '../styles/custom.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp ({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component { ...pageProps } />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/about/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import About from '../../Components/PageComponents/About';
+
+export default () => {
+    return <About />;
+}
